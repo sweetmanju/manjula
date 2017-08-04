2b55ab8d6317013e092af02817f6d736020d72aa
sweetmanju <sweetmanju110@gmail.com>
2017-08-04 11:42:56 +0530

    Add files via upload

---
+++ indexnum.java
@@ -0,0 +1,23 @@
+package mile2practise;
+import java.util.Scanner;
+import java.util.Arrays;
+public class indexnum {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		Arrays.sort(a);
+		for(int i=0;i<n;i++)
+		{
+			if(i==a[i])
+				System.out.println(a[i]);
+		}
+		}
+
+	}
+
